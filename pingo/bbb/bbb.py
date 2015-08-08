8f42fe4a1ed1a2b31e65e2454ed7d641bb50ec5f
Dyego Cantu <c4ntuz@gmail.com>
2015-08-08 12:32:42 -0300

    Using the Adafruit_BBIO library

--- pingo/bbb/bbb.py
+++ pingo/bbb/bbb.py
@@ -4,6 +4,14 @@ from pingo.board import Board
 class BeagleBoneBlack(Board):
 
     def __init__(self):
+        global GPIO
+        try:
+            import Adafruit_BBIO.GPIO as GPIO
+        except ImportError:
+            raise ImportError(
+                    'pingo.bbb.BeagleBoneBlack requires Adafruit_BBIO installed'
+                    )
+
         super(BeagleBoneBlack, self).__init__()
 
     def cleanup(self):
@@ -13,4 +21,7 @@ class BeagleBoneBlack(Board):
         pass
 
     def _set_pin_state(self, pin, state):
+        pass
+
+    def _get_pin_state(self, pin):
         pass
